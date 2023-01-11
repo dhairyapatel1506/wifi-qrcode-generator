deda19c540d3cd50de7c31d25997d93121682393
dhairyapatel1506 <ts554408@gmail.com>
2023-01-11 22:56:50 +0530

    Now works on Windows too!

--- wifiqrcodegen.py
+++ wifiqrcodegen.py
@@ -1,22 +1,36 @@
 #Import modules
+import platform
 import subprocess
 import wifi_qrcode_generator
 
-print("Saved Wifi Networks:\n")
-savedNetworks = subprocess.check_output(['ls', '/etc/NetworkManager/system-connections/']).decode('utf-8').split("\n")
-for savedNetwork in savedNetworks:
-    print(f"\t{savedNetwork.split('.')[0]}")
+os = platform.system()
 
-ssid = input("Enter SSID: ")
+print("Saved Wifi Networks:\n")
+if(os=="Linux"):
+    # Lists Network names
+    savedNetworks = subprocess.check_output(['ls', '/etc/NetworkManager/system-connections/']).decode('utf-8').split("\n")
+    for network in savedNetworks:
+        print(f"\t{network.split('.nmconnection')[0]}")
+    ssid = input("Enter SSID: ")
+else:
+    savedNetworks = subprocess.check_output(['netsh', 'wlan', 'show', 'profile']).decode('utf-8').split("\n")
+    for network in savedNetworks:
+        if("All User Profile" in network):
+            print(f"\t{network.split(':')[1]}")
+    ssid = input("\nEnter SSID: ")
  
 try:
-   
-    # Retrieve saved wifi data
-    Id = subprocess.check_output(
-        ['sudo', 'cat', f"/etc/NetworkManager/system-connections/{ssid}.nmconnection"]).decode('utf-8').split('\n')
-     
-    pwd = str([b.split("=")[1]
-        for b in Id if "psk=" in b])[2:-2]
+    if(os=="Linux"): 
+        # Retrieves Network Profile and extracts Password
+        profile = subprocess.check_output(
+            ['sudo', 'cat', f"/etc/NetworkManager/system-connections/{ssid}.nmconnection"]).decode('utf-8').split('\n')
+         
+        pwd = str([b.split("=")[1]
+            for b in profile if "psk=" in b])[2:-2]
+    else:
+        profile = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', ssid, 'key=clear']).decode('utf-8').split('\n')
+
+        pwd = str([b.split(':')[1].strip() for b in profile if "Key Content" in b])[2:-2]
 
     print("Password :", pwd)
      
@@ -24,8 +38,8 @@ except:
 
     print("Something's wrong")
      
-# Generate QR code
+# Generates QR code
 picture = wifi_qrcode_generator.wifi_qrcode(ssid, False, 'WPA', pwd)
 picture = picture.save(f"{ssid}.png")
 
-print("The QR code was succesfully generated and has been stored in the current directory.")
+print("The QR code has been stored in the current directory.")
